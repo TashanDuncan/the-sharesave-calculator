92ebbff5f9ae1f01f8c3d2bcf190c6b1e944206d
viewtifult <tashanduncan1994@gmail.com>
2020-11-11 22:41:23 +0000

    create IDs for paragraphs to select via DOM.js

--- js/dom.js
+++ js/dom.js
@@ -1,2 +1,9 @@
 const submit = document.getElementById('submit')
+const returnText = document.getElementById('return')
+const savedText = document.getElementById('saved')
+const profitText = document.getElementById('profit')
 
+submit.addEventListener('click', () => {
+
+
+})
