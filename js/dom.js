8a769bae4298b5cbfdd4c2435eb73f5016edbddb
viewtifult <tashanduncan1994@gmail.com>
2020-11-11 22:57:45 +0000

    add event listener to input data

--- js/dom.js
+++ js/dom.js
@@ -2,8 +2,14 @@ const submit = document.getElementById('submit')
 const returnText = document.getElementById('return')
 const savedText = document.getElementById('saved')
 const profitText = document.getElementById('profit')
+const percentageText = document.getElementById('percent')
+const optionsText = document.getElementById('options')
 
 submit.addEventListener('click', () => {
 
-
+    returnText.textContent = `${convertToCurrency(estimatedReturn())}`;
+    savedText.textContent = `${convertToCurrency(totalSavings())}`;
+    profitText.textContent =`${convertToCurrency(estimatedProfit())}`;
+    percentageText.textContent = `${percentIncreased()}`;
+    optionsText.textContent = `${numberOfOptions()}`;
 })
