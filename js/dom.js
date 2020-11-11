cf0e1e0f300100c0f2ae29451b07b7bc8e2a7a89
viewtifult <tashanduncan1994@gmail.com>
2020-11-11 23:47:45 +0000

    added feature to display results only when calculation button is clicked

--- js/dom.js
+++ js/dom.js
@@ -5,10 +5,11 @@ const savedText = document.getElementById('saved')
 const profitText = document.getElementById('profit')
 const percentageText = document.getElementById('percent')
 const optionsText = document.getElementById('options')
+const resultsDiv = document.getElementById('results')
 
 // Event Listeners
 submit.addEventListener('click', () => {
-
+    resultsDiv.classList.remove('d-none');
     returnText.textContent = `${convertToCurrency(estimatedReturn())}`;
     savedText.textContent = `${convertToCurrency(totalSavings())}`;
     profitText.textContent =`${convertToCurrency(estimatedProfit())}`;
