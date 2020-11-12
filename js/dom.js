101391f59d6d619e744d58d01b6e0e65860c18cc
viewtifult <tashanduncan1994@gmail.com>
2020-11-12 00:00:29 +0000

    Fix issue where submiting calculation would  refresh the page

--- js/dom.js
+++ js/dom.js
@@ -1,4 +1,5 @@
 //DOM Variables
+const form = document.getElementById('myForm')
 const submit = document.getElementById('submit')
 const returnText = document.getElementById('return')
 const savedText = document.getElementById('saved')
@@ -7,8 +8,13 @@ const percentageText = document.getElementById('percent')
 const optionsText = document.getElementById('options')
 const resultsDiv = document.getElementById('results')
 
+//Functions
+function handleForm(event) { event.preventDefault(); } 
+
 // Event Listeners
-submit.addEventListener('click', () => {
+form.addEventListener('submit', handleForm);
+
+form.addEventListener('submit', () => {
     resultsDiv.classList.remove('d-none');
     returnText.textContent = `${convertToCurrency(estimatedReturn())}`;
     savedText.textContent = `${convertToCurrency(totalSavings())}`;
