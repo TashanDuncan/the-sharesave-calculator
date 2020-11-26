a87b09c6a1db453a0ab2cee2404a5d737e13a96d
viewtifult <tashanduncan1994@gmail.com>
2020-11-26 23:01:11 +0000

    improved calculate tax to not give negative values

--- js/calculations.js
+++ js/calculations.js
@@ -30,9 +30,14 @@ const convertToCurrency = (num) =>  `£${Number.parseFloat(num).toFixed(2)}`
 const percentIncreased = () => `${Number.parseFloat((estimatedReturn() - totalSavings()) / totalSavings()).toFixed(2) * 100}%`
 
 const calculateTax = () => {
-   const taxFree = estimatedReturn() - taxableIncome;
-
-   return taxFree / 10;
+    let taxFree;
+    if (estimatedReturn() > taxableIncome){
+        taxFree = estimatedReturn() - taxableIncome;
+        return taxFree / 10;
+    } else {
+        return 0;
+    }
+   
 }
 
 const grandTotal = () => estimatedReturn() - calculateTax ();
