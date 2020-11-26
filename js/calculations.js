1e52d276941a57120c6069478c66354d7a457661
viewtifult <tashanduncan1994@gmail.com>
2020-11-26 23:15:26 +0000

    converted arrow functions to names functions to manage hoisting easier

--- js/calculations.js
+++ js/calculations.js
@@ -1,13 +1,13 @@
 const taxableIncome = 12300;
 
 // Obtain total savings based on monthly saving * duration of savings
-const totalSavings = () =>{
+function totalSavings() {
     const savingsPerMonth = document.getElementById("monthly-savings").value;
     let yearlyTerm;
-    
-    if (document.getElementById('saving-3-years').checked){
+
+    if (document.getElementById('saving-3-years').checked) {
         yearlyTerm = 36;
-    } else if (document.getElementById('saving-5-years').checked){
+    } else if (document.getElementById('saving-5-years').checked) {
         yearlyTerm = 60;
     }
     const totalSavings = savingsPerMonth * yearlyTerm;
@@ -15,29 +15,39 @@ const totalSavings = () =>{
 }
 
 
-const numberOfOptions = () => {
+function numberOfOptions() {
     const options = document.getElementById("option-price").value;
     const result = Number.parseInt(totalSavings() / options * 100);
     return result;
 }
 
-const estimatedReturn = () => numberOfOptions() * document.getElementById('estimate-price').value / 100;
+function estimatedReturn() {
+    return numberOfOptions() * document.getElementById('estimate-price').value / 100;
+}
 
-const estimatedProfit = () => estimatedReturn() - totalSavings();
+function estimatedProfit() {
+    return estimatedReturn() - totalSavings();
+}
 
-const convertToCurrency = (num) =>  `£${Number.parseFloat(num).toFixed(2)}`
+function convertToCurrency(num) {
+    return `£${Number.parseFloat(num).toFixed(2)}`;
+}
 
-const percentIncreased = () => `${Number.parseFloat((estimatedReturn() - totalSavings()) / totalSavings()).toFixed(2) * 100}%`
+function percentIncreased() {
+    return `${Number.parseFloat((estimatedReturn() - totalSavings()) / totalSavings()).toFixed(2) * 100}%`;
+}
 
-const calculateTax = () => {
+function calculateTax() {
     let taxFree;
-    if (estimatedProfit() > taxableIncome){
+    if (estimatedProfit() > taxableIncome) {
         taxFree = estimatedProfit() - taxableIncome;
         return taxFree / 10;
     } else {
         return 0;
     }
-   
+
 }
 
-const grandTotal = () => estimatedReturn() - calculateTax ();
+function grandTotal() {
+    return estimatedReturn() - calculateTax();
+}
